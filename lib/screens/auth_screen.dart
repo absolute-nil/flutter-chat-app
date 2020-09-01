eee68e605be63490981c30303164f3cbae370b4c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-01 19:35:53 +0530

    create auth screen

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'file:///C:/Users/Nikhil/Desktop/Flutter%20Projects/chat_app/lib/widgets/auth/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm()
+    );
+  }
+}
