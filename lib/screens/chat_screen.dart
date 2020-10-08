6d690e5b9e3e4f638a7ff995105b1958d16ed8fb
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-10-08 11:55:11 +0530

    hook message widgeet and new message widget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat/messages.dart';
+import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,63 +8,42 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Chat App'),
-          actions: [
-            DropdownButton(
-                icon: Icon(
-                  Icons.more_vert,
-                  color: Theme.of(context).primaryIconTheme.color,
-                ),
-                items: [
-                  DropdownMenuItem(
-                    child: Container(
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.exit_to_app),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text('Logout')
-                        ],
-                      ),
+      appBar: AppBar(
+        title: Text('Chat App'),
+        actions: [
+          DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.exit_to_app),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('Logout')
+                      ],
                     ),
-                    value: 'logout',
                   ),
-                ],
-                onChanged: (itemIdentifier) {
-                  if (itemIdentifier == 'logout') {
-                    FirebaseAuth.instance.signOut();
-                  }
-                })
-          ],
+                  value: 'logout',
+                ),
+              ],
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              })
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [Expanded(child: Messages()), NewMessage()],
         ),
-        body: StreamBuilder(
-            stream: Firestore.instance
-                .collection("chats/o0ys8srxX6b0WXl9GuUt/messages")
-                .snapshots(),
-            builder: (ctx, streamSnapShot) {
-              if (streamSnapShot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              final documents = streamSnapShot.data.documents;
-
-              return ListView.builder(
-                  itemCount: documents.length,
-                  itemBuilder: (ctx, index) => Container(
-                        padding: EdgeInsets.all(8),
-                        child: Text(documents[index]['text']),
-                      ));
-            }),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Firestore.instance
-                .collection("chats/o0ys8srxX6b0WXl9GuUt/messages")
-                .add({'text': 'this was added by click'});
-          },
-          child: Icon(Icons.add),
-        ));
+      ),
+    );
   }
 }
