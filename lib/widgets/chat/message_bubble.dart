8dde17241fbe3b71292465f6fa7df7f6d06eb861
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-10-14 11:10:17 +0530

    fetch username and ifx alignment of chats

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final String userId;
   final bool isMe;
   final Key key;
 
-  MessageBubble(this.message, this.isMe, {this.key});
+  MessageBubble(this.message, this.userId, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +27,44 @@ class MessageBubble extends StatelessWidget {
             width: 140,
             padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
             margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: Text(
-              message,
-              style: TextStyle(
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).accentTextTheme.headline1.color),
-            ),
+            child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  FutureBuilder(
+                      future: Firestore.instance
+                          .collection('users')
+                          .document(userId)
+                          .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Text('Loading...');
+                        }
+                        return Text(
+                          snapshot.data['username'],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: isMe
+                                  ? Colors.black
+                                  : Theme.of(context)
+                                      .accentTextTheme
+                                      .headline1
+                                      .color),
+                        );
+                      }),
+                  Text(
+                    message,
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    style: TextStyle(
+                        color: isMe
+                            ? Colors.black
+                            : Theme.of(context)
+                                .accentTextTheme
+                                .headline1
+                                .color),
+                  ),
+                ]),
           ),
         ]);
   }
